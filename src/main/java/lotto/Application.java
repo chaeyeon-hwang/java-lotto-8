ea7c5d0a50ddbe629657f43e4bf5aa201c99781f
chaeyeon-hwang <chaeyeon9034@naver.com>
2025-11-03 17:40:05 +0900

    feat(winning): WinningLotto 클래스 당첨 번호와 보너스 번호 검증 로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,42 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            new Application().run();
+        } finally {
+            camp.nextstep.edu.missionutils.Console.close();
+        }
+    }
+
+    private void run() {
+        int purchaseAmount = retryUntilValid(InputView::readPurchaseAmount);
+        List<Lotto> lottos = LottoMachine.buy(purchaseAmount);
+        OutputView.printPurchased(lottos);
+
+        WinningLotto winningLotto = retryUntilValid(InputView::readWinningLotto);
+        int bonus = retryUntilValid(InputView::readBonusNumber);
+        winningLotto = winningLotto.withBonus(bonus);
+
+        ResultCalculator.Result result = ResultCalculator.calculate(lottos, winningLotto, purchaseAmount);
+        OutputView.printStatistics(result);
+    }
+
+    private <T> T retryUntilValid(SupplierWithException<T> action) {
+        while (true) {
+            try {
+                return action.get();
+            } catch (IllegalArgumentException | IllegalStateException e) {
+                ExceptionHandler.print(e);
+            }
+        }
+    }
+
+    @FunctionalInterface
+    private interface SupplierWithException<T> {
+        T get();
     }
 }
